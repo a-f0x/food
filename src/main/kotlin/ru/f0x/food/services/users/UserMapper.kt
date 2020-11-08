21f9243d5353096ca573f4bd0275f912c6f7877f
f0x <af0xdev@gmail.com>
2020-11-08 23:50:52 +0700

    refactor

--- src/main/kotlin/ru/f0x/food/services/users/UserMapper.kt
+++ src/main/kotlin/ru/f0x/food/services/users/UserMapper.kt
@@ -1,30 +1,36 @@
 package ru.f0x.food.services.users
 
 import org.springframework.stereotype.Component
+import ru.f0x.food.models.dto.users.NutrientsResponse
+import ru.f0x.food.models.dto.users.ProfileResponse
 import ru.f0x.food.models.dto.users.UserDTO
 import ru.f0x.food.models.dto.users.UserProfileDTO
 import ru.f0x.food.models.entity.RoleEntity
 import ru.f0x.food.models.entity.UserEntity
 import ru.f0x.food.models.entity.UserProfileEntity
+import ru.f0x.food.services.calculator.TargetCalculator
 import java.time.LocalDateTime
 
 @Component
 class UserMapper {
 
-    fun mapFromEntity(userEntity: UserEntity, profileEntity: UserProfileEntity, password: String?): UserDTO = UserDTO(
-            id = userEntity.id,
-            password = password,
-            email = userEntity.email,
-            profile = UserProfileDTO(
-                    profileEntity.id,
-                    profileEntity.sex,
-                    profileEntity.weight,
-                    profileEntity.height,
-                    profileEntity.age,
-                    profileEntity.activity,
-                    profileEntity.target
-            )
-    )
+    fun mapFromEntity(profile: UserProfileEntity, email: String, password: String?): ProfileResponse {
+        val result = calculateNutrients(profile)
+        return ProfileResponse(
+                password,
+                email,
+                profile.sex,
+                profile.weight,
+                profile.height,
+                profile.age,
+                profile.activity,
+                profile.target,
+                result.basicKCal,
+                result.totalKCal,
+                result.nutrientResponse
+        )
+
+    }
 
     fun mapFromDTO(dto: UserDTO, password: String, time: LocalDateTime, role: RoleEntity): UserEntity =
             UserEntity().apply {
@@ -47,4 +53,51 @@ class UserMapper {
                 this.created = time
                 this.modified = time
             }
+
+
+    private fun calculateNutrients(profile: UserProfileEntity): CalcResult {
+        val result = TargetCalculator(
+                profile.weight,
+                profile.height,
+                profile.age,
+                profile.activity,
+                profile.sex
+        ).calculate(profile.target)
+
+        val proteins = NutrientsResponse(
+                result.nutrients.proteins.name,
+                result.nutrients.proteins.weightGram,
+                result.nutrients.proteins.kCal,
+                result.nutrients.proteins.type.kCalPerGram
+        )
+        val fats = NutrientsResponse(
+                result.nutrients.fats.name,
+                result.nutrients.fats.weightGram,
+                result.nutrients.fats.kCal,
+                result.nutrients.fats.type.kCalPerGram
+        )
+        val carbohydrates = NutrientsResponse(
+                result.nutrients.carbohydrates.name,
+                result.nutrients.carbohydrates.weightGram,
+                result.nutrients.carbohydrates.kCal,
+                result.nutrients.carbohydrates.type.kCalPerGram
+        )
+
+        return CalcResult(
+                result.basicKCal,
+                result.nutrients.totalKCal,
+                mapOf(
+                        proteins.name to proteins,
+                        fats.name to fats,
+                        carbohydrates.name to carbohydrates
+                )
+        )
+    }
+
+    private data class CalcResult(
+            val basicKCal: Float,
+            val totalKCal: Float,
+            val nutrientResponse: Map<String, NutrientsResponse>
+
+    )
 }
--- src/main/kotlin/ru/f0x/food/services/users/UserService.kt
+++ src/main/kotlin/ru/f0x/food/services/users/UserService.kt
@@ -5,16 +5,13 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import ru.f0x.food.models.dto.CustomUserDetails
 import ru.f0x.food.models.dto.users.CreateUserDTO
-import ru.f0x.food.models.dto.users.NutrientsResponse
 import ru.f0x.food.models.dto.users.ProfileResponse
 import ru.f0x.food.models.entity.RoleEntity
 import ru.f0x.food.models.entity.RoleEnum
-import ru.f0x.food.models.entity.UserProfileEntity
 import ru.f0x.food.repository.RolesRepository
 import ru.f0x.food.repository.UserProfileRepository
 import ru.f0x.food.repository.UsersRepository
 import ru.f0x.food.services.IDateTimeService
-import ru.f0x.food.services.calculator.TargetCalculator
 
 @Service
 class UserService(
@@ -51,88 +48,13 @@ class UserService(
         val profile = profileRepository.save(
                 mapper.mapFromDTO(createUserDTO.profile, currentTime, user.id)
         )
-        val result = calculateNutrients(profile)
-
-        return ProfileResponse(
-                password,
-                user.email,
-                profile.sex,
-                profile.weight,
-                profile.height,
-                profile.age,
-                profile.activity,
-                profile.target,
-                result.basicKCal,
-                result.totalKCal,
-                result.nutrientResponse
-        )
+        return mapper.mapFromEntity(profile, user.email, password)
     }
 
     override fun getUserProfile(user: CustomUserDetails): ProfileResponse {
         val profile = profileRepository.findByUserId(user.id)
-        val result = calculateNutrients(profile)
-        return ProfileResponse(
-                null,
-                user.email,
-                profile.sex,
-                profile.weight,
-                profile.height,
-                profile.age,
-                profile.activity,
-                profile.target,
-                result.basicKCal,
-                result.totalKCal,
-                result.nutrientResponse
-        )
-
-    }
-
-    private fun calculateNutrients(profile: UserProfileEntity): CalcResult {
-        val result = TargetCalculator(
-                profile.weight,
-                profile.height,
-                profile.age,
-                profile.activity,
-                profile.sex
-        ).calculate(profile.target)
-
-        val proteins = NutrientsResponse(
-                result.nutrients.proteins.name,
-                result.nutrients.proteins.weightGram,
-                result.nutrients.proteins.kCal,
-                result.nutrients.proteins.type.kCalPerGram
-        )
-        val fats = NutrientsResponse(
-                result.nutrients.fats.name,
-                result.nutrients.fats.weightGram,
-                result.nutrients.fats.kCal,
-                result.nutrients.fats.type.kCalPerGram
-        )
-        val carbohydrates = NutrientsResponse(
-                result.nutrients.carbohydrates.name,
-                result.nutrients.carbohydrates.weightGram,
-                result.nutrients.carbohydrates.kCal,
-                result.nutrients.carbohydrates.type.kCalPerGram
-        )
-
-        return CalcResult(
-                result.basicKCal,
-                result.nutrients.totalKCal,
-                mapOf(
-                        proteins.name to proteins,
-                        fats.name to fats,
-                        carbohydrates.name to carbohydrates
-                )
-        )
+        return mapper.mapFromEntity(profile, user.email, null)
     }
 
     private fun getCurrentTime() = dateTimeService.getCurrentTime()
-
-    private data class CalcResult(
-            val basicKCal: Float,
-            val totalKCal: Float,
-            val nutrientResponse: Map<String, NutrientsResponse>
-
-    )
-
 }
