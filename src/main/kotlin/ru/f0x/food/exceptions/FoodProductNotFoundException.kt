0308f46ae49ef19e5e9a4131be2bf40296b1b6f1
f0x <af0xdev@gmail.com>
2020-11-09 00:24:09 +0700

    refactor

--- src/main/kotlin/ru/f0x/food/exceptions/FoodProductNotFoundException.kt
+++ src/main/kotlin/ru/f0x/food/exceptions/FoodProductNotFoundException.kt
@@ -4,6 +4,6 @@ class FoodProductNotFoundException(nutrientId: Int) : Exception() {
     val details: Map<String, Any> = mapOf("id" to nutrientId)
     override val message: String
         get() {
-            return "Nutrient not found"
+            return "Food product not found"
         }
 }
